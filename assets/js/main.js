a7682d24616ff445d6ce6018afe9a6059a5e729b
madonnasantorosario <79143559+madonnasantorosario@users.noreply.github.com>
2021-02-21 22:10:13 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+    
+    document.getElementById("cover").style.textShadow = "1px 1px 1px #000000";
+ }
+
+function sitostorico(){
+    
+	location.href = "https://confraternitasantorosario.jimdofree.com/";
+    /*location.href = "https://sitostorico.madonnasantorosario.it";*/
+} 
