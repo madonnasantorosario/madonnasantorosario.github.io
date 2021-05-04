0c7e758139b64031e762a4b80affe2867b2e803c
madonnasantorosario <79143559+madonnasantorosario@users.noreply.github.com>
2021-05-04 14:18:42 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,7 @@ function sitostorico(){
 	 window.open("https://www.sitostorico.madonnasantorosario.it/");
 	 /*window.open("https://confraternitasantorosario.jimdofree.com/");*/
 } 
+
+function avviso() {
+    alert("i canali social sono temporaneamente disabilitati");
+ }
